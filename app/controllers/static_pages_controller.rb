36f09aa215548bdbcfe4d22bdec17f521be8d0f8
Tomas <jtomas1993@gmail.com>
2014-02-06 23:12:09 +0200

    add navbar and javascript

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    params[:active] = 'home'
   end
 
   def help
+    params[:active] = 'help'
   end
   
   def about
+    params[:active] = 'about'
   end
 end
