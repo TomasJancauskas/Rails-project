317a96c122a145d6dacf0599881f8b79ca394d67
Tomas <jtomas1993@gmail.com>
2014-02-10 18:54:06 +0200

    Remove 2 static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,4 @@ class StaticPagesController < ApplicationController
   def home
     params[:active] = 'home'
   end
-
-  def help
-    params[:active] = 'help'
-  end
-  
-  def about
-    params[:active] = 'about'
-  end
 end
