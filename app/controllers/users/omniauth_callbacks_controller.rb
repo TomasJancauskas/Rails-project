49702adef82295882ebabdd563b90f13fa489e60
Tomas Jančauskas <jtomas1993@gmail.com>
2014-02-20 23:45:43 +0200

    Fnish Sing Up as github

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_github_oauth(request.env["omniauth.auth"])
 
+    if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?
-   
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url, :flash => { :alert => "Account with this email already exists"}
+    end
   end
 end
