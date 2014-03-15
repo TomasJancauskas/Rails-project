574ddcdcc743787203463dee3aceda6477ec1473
Tomas Jančauskas <jtomas1993@gmail.com>
2014-03-15 21:47:35 +0200

    show commit count

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   end
   def search
     @user = Octokit.user params[:q]
+    @rel = Octokit.commits("tomasjancauskas/easyMetrics", "Development", :since => "2012-09-28T00:00:00+00:00").length
   end
 end
